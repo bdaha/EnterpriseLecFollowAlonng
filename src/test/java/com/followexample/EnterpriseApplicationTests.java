57e6faf245d10a7f69f41f10fd77eacac129cb61
Binod Dahal <60723232+bdaha@users.noreply.github.com>
2022-01-17 14:43:06 -0500

    Initial commit

---
+++ src/test/java/com/followexample/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.followexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
